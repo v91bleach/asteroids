930b7d16319cbe4ebe0e5e1be26273fdd67a1416
v91bleach <v91.bleach@gmail.com>
2026-01-21 09:14:26 -0600

    end chapter 2

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     # Start game loop
     while True:
         log_state()
@@ -17,6 +20,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
